9be5bd940112d0cc2152b7dd12764e1ae203fa11
thailandvista7-crypto <thailandvista7@gmail.com>
2026-04-19 08:32:31 +0700

    Add HelmetProvider for managing document head

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { httpBatchLink } from '@trpc/client'
-import { trpc } from './trpc/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { httpBatchLink } from '@trpc/client';
+import { trpc } from './trpc/client';
+import './index.css';
+import App from './App.tsx';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,11 +28,13 @@ const trpcClient = trpc.createClient({
 });
 
 createRoot(document.getElementById('root')!).render(
-  <trpc.Provider client={trpcClient} queryClient={queryClient}>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </trpc.Provider>,
-)
+  <HelmetProvider>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </trpc.Provider>
+  </HelmetProvider>,
+);
